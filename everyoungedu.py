86a571f17e02e6b64e3c4e16fe089d3382067d40
leeym114 <84678701+leeym114@users.noreply.github.com>
2024-11-08 13:03:28 +0900

    Update everyoungedu.py

--- everyoungedu.py
+++ everyoungedu.py
@@ -1,8 +1,8 @@
 # streamlit run ./streamlit/everyoungedu.py
 
 import streamlit as st
-from selenium.common.exceptions import NoSuchWindowException, WebDriverException
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchWindowException, WebDriverException
 import time
 
 def playTime_display(playstatus, playtime):
